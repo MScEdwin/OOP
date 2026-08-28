f9f73313ff7bfcbbda91cfb8d28aae01675b7494
MScEdwin <eduardoat007@gmail.com>
2026-08-27 23:00:58 -0500

    Add files via upload

---
+++ Practice Problem-01.py
@@ -0,0 +1,10 @@
+name=input("Digit your name: ")
+
+wages=int(input("Digit how many wages: "))
+hours=int(input("Digit how many hours: "))
+days=int(input("Digit how many days: "))
+
+total_wages= wages*hours*days
+
+print("The user", name,"has a total wages of:", total_wages)
+
