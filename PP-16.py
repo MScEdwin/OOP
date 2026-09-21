65bc630c44928018660e33a32e64f4d1ba4b6595
MScEdwin <eduardoat007@gmail.com>
2026-09-20 22:12:31 -0500

    Refactor student management logic and add error checks
    Improved student management program with error handling.

--- PP-16.py
+++ PP-16.py
@@ -1,7 +1,7 @@
-students={}
-i=1
+students = {}
+i = 1
 
-while (1):
+while True:
     print("1. Add a Student")
     print("2. Remove a Student")
     print("3. Edit a Student")
@@ -9,25 +9,41 @@ while (1):
     print("5. Exit")
 
     choice = int(input("Enter your choice: "))
-
     if choice == 1:
-        name=input("Enter your name: ")
-        major=input("Enter your major: ")
-        year=input("Enter your year: ")
-        students.update({"S"+str(i):{"name":name,
-                                "major":major,
-                                "year":year}})
-    if choice == 2:
-        que="S"+str(input("Digit the number of the student you want to remove: "))
-        students.pop(que)
-    if choice == 3:
-        que = "S" + str(input("Digit the number of the student you want to edit: "))
+        name = input("Enter your name: ")
+        major = input("Enter your major: ")
+        year = input("Enter your year: ")
+        students.update({"S" + str(i):{
+            "name": name,
+            "major": major,
+            "year": year}})
+        i = i + 1
+    elif choice == 2:
+        num = input("Digit the number of the student you want to remove: ")
+        que = "S" + num
         if que in students:
-            newelemet=input("Enter the new element of the student you want to edit(Name / Major / Year): ")
-            Val=input("Enter the new element of the student you want to change: ")
-            students.update{que:{newelemet:val}
-    if choice == 4:
+            students.pop(que)
+        else:
+            print("Error: Student ID does not exist.")
+    elif choice == 3:
+        num = input("Digit the number of the student you want to edit: ")
+        que = "S" + num
+        if que in students:
+            newelemet = input("Enter the element you want to edit (name / major / year): ")
+            if newelemet in ["name", "major", "year"]:
+                val = input("Enter the new value: ")
+                students[que][newelemet] = val
+                print("Student updated successfully.")
+            else:
+                print("Error: Invalid field name entered.")
+        else:
+            print("Error: Student ID not found.")
+    elif choice == 4:
+        print("\n--- Student Directory ---")
         print(students)
-    if choice == 5:
+
+    elif choice == 5:
+        print("Thanks for using the program")
         break
-    i=i+1
+    else:
+        print("Invalid choice. Please select between 1 and 5.")
